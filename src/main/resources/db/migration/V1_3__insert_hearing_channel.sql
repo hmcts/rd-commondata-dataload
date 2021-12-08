ffdafe526b8e8e12c1ca535de18a657d1f433df3
Sourav Roy <sourav.roy@hmcts.net>
2021-12-08 13:58:17 +0000

    Create and insert sql for table hearing_priority

--- src/main/resources/db/migration/V1_3__insert_hearing_channel.sql
+++ src/main/resources/db/migration/V1_3__insert_hearing_channel.sql
@@ -1354,6 +1354,26 @@ INSERT INTO hearing_type (CategoryKey,ServiceID,Key,Value_EN,Value_CY,HintText_E
 ('HearingType', 'BBA3', 'BBA3-directionHearings', 'Direction Hearings', null, null, null, 2, null, null, 'Y'),
 ('HearingType', 'BBA3', 'BBA3-chambersOutcome', 'Chambers Outcome', null, null, null, 3, null, null, 'Y');
 
+CREATE TABLE hearing_priority (
+CategoryKey varchar(64) NOT NULL,
+ServiceID	varchar(16),
+Key	varchar(64)	NOT NULL,
+Value_EN varchar(128) NOT NULL,
+Value_CY varchar(128),
+HintText_EN	varchar(512),
+HintText_CY	varchar(512),
+Lov_Order BIGINT,
+ParentCategory varchar(64),
+ParentKey	varchar(64),
+Active varchar(1)
+);
+
+INSERT INTO hearing_priority (CategoryKey,ServiceID,Key,Value_EN,Value_CY,HintText_EN,HintText_CY,Lov_Order,ParentCategory,ParentKey,Active)
+VALUES ('HearingPriority', null, 'normal', 'Normal', null, null , null, null, null, null, 'Y'),
+('HearingPriority', null, 'high', 'High', null, null, null, null, null, null, 'Y'),
+('HearingPriority', null, 'critical', 'Critical', null, null, null, null, null, null, 'Y'),
+('HearingPriority', null, 'pending', 'Pending', null, null, null, null, null, null, 'Y');
+
 CREATE TABLE flag_service(
 	id bigint NOT NULL,
 	service_id varchar(16) NOT NULL,
