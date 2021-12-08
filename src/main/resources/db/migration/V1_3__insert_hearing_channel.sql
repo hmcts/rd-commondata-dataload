08771a208c924640e9111ee5007880bbb9a6ff18
Manish Paryani <85675203+ManishParyani@users.noreply.github.com>
2021-12-08 09:18:07 +0000

    Added Flag_service&Audit&Exception Table

--- src/main/resources/db/migration/V1_3__insert_hearing_channel.sql
+++ src/main/resources/db/migration/V1_3__insert_hearing_channel.sql
@@ -1353,3 +1353,37 @@ INSERT INTO hearing_type (CategoryKey,ServiceID,Key,Value_EN,Value_CY,HintText_E
 ('HearingType', 'BBA3', 'BBA3-substantive', 'Substantive', null, null , null, 1, null, null, 'Y'),
 ('HearingType', 'BBA3', 'BBA3-directionHearings', 'Direction Hearings', null, null, null, 2, null, null, 'Y'),
 ('HearingType', 'BBA3', 'BBA3-chambersOutcome', 'Chambers Outcome', null, null, null, 3, null, null, 'Y');
+
+CREATE TABLE flag_service(
+	id bigint NOT NULL,
+	service_id varchar(16) NOT NULL,
+	hearing_relevant boolean NOT NULL,
+	request_reason boolean NOT NULL,
+	flag_code varchar(16) NOT NULL,
+	constraint flag_service_pk primary key (id));
+
+ALTER TABLE flag_service add CONSTRAINT flag_service_flag_code_fk FOREIGN KEY (flag_code)
+REFERENCES flag_details (flag_code);
+
+CREATE TABLE dataload_exception_records(
+ id SERIAL NOT NULL,
+ table_Name varchar(64),
+ scheduler_name varchar(64) NOT NULL,
+ scheduler_start_time timestamp NOT NULL,
+ key varchar(256),
+ field_in_error varchar(256),
+ error_description varchar(512),
+ updated_timestamp timestamp NOT NULL,
+ row_id bigint,
+ CONSTRAINT dataload_exception_records_pk PRIMARY KEY (ID)
+);
+
+CREATE TABLE dataload_schedular_audit(
+  id serial NOT NULL,
+  scheduler_name varchar(64) NOT NULL,
+  file_name varchar(128),
+  scheduler_start_time timestamp NOT NULL,
+  scheduler_end_time timestamp,
+  status varchar(32),
+  CONSTRAINT dataload_schedular_audit_pk PRIMARY KEY (id)
+);
