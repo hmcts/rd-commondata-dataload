524e70230c47314967ff22fb682aeb3aeab54ac6
LukeHalsall <luke.halsall@HMCTS.net>
2021-12-16 14:23:16 +0000

    added entity role codes insert and table

--- src/main/resources/db/migration/V1_5__insert_additional_facilities.sql
+++ src/main/resources/db/migration/V1_5__insert_additional_facilities.sql
@@ -24,3 +24,23 @@ INSERT INTO additional_facilities (categorykey,serviceid,"key",value_en,value_cy
 	 ('AdditionalFacilities',NULL,'AF-PVL','Prison Video Link',NULL,NULL,NULL,NULL,NULL,NULL,'Y');
 
 UPDATE interpreter_and_sign_language SET value_en='Belorussian' WHERE value_en='Belorussia';
+
+CREATE TABLE entity_role_codes (
+  categorykey varchar(64) NOT NULL,
+  serviceid varchar(16),
+  key varchar(64) NOT NULL,
+  value_en varchar(128) NOT NULL,
+  value_cy varchar(128),
+  hinttext_en varchar(512),
+  hinttext_cy varchar(512),
+  lov_order BIGINT,
+  parentcategory varchar(64),
+  parentkey varchar(64),
+  active varchar(1)
+);
+
+INSERT INTO entity_role_codes (categorykey,serviceid,"key",value_en,value_cy,hinttext_en,hinttext_cy,lov_order,parentcategory,parentkey,active) VALUES
+ ('EntityRoleCode', 'BBA3', 'BBA3-appellant', 'Appellant', null, null, null, null, null, null, 'Y'),
+ ('EntityRoleCode', 'BBA3', 'BBA3-appointee', 'Appointee', null, null, null, null, null, null, 'Y'),
+ ('EntityRoleCode', 'BBA3', 'BBA3-jointParty', 'Joint Party', null, null, null, null, null, null, 'Y'),
+ ('EntityRoleCode', 'BBA3', 'BBA3-otherParty', 'Other Party', null, null, null, null, null, null, 'Y');
