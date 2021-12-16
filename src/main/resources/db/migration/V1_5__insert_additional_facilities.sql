eec91d94e45c2c5839ee252a10bd4c6324fd6778
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2021-12-16 10:14:34 +0000

    Create and Insert Additional Facilities table

--- src/main/resources/db/migration/V1_5__insert_additional_facilities.sql
+++ src/main/resources/db/migration/V1_5__insert_additional_facilities.sql
@@ -1,15 +1,15 @@
-CREATE TABLE Additional_Facilities (
-CategoryKey varchar(64) not null,
-ServiceID varchar(16),
-Key varchar(64) not null,
-Value_EN varchar(128) not null,
-Value_CY varchar(128),
-HintText_EN varchar(512),
-HintText_CY varchar(512),
-Lov_Order BIGINT,
-ParentCategory varchar(64),
-ParentKey varchar(64),
-Active varchar(1)
+CREATE TABLE additional_facilities (
+	categorykey varchar(64) NOT NULL,
+	serviceid varchar(16) NULL,
+	"key" varchar(64) NOT NULL,
+	value_en varchar(128) NOT NULL,
+	value_cy varchar(128) NULL,
+	hinttext_en varchar(512) NULL,
+	hinttext_cy varchar(512) NULL,
+	lov_order int8 NULL,
+	parentcategory varchar(64) NULL,
+	parentkey varchar(64) NULL,
+	active varchar(1) NULL
 );
 
 INSERT INTO additional_facilities (categorykey,serviceid,"key",value_en,value_cy,hinttext_en,hinttext_cy,lov_order,parentcategory,parentkey,active) VALUES
@@ -21,5 +21,7 @@ INSERT INTO additional_facilities (categorykey,serviceid,"key",value_en,value_cy
 	 ('AdditionalFacilities',NULL,'AF-WR','Witness Room',NULL,NULL,NULL,NULL,NULL,NULL,'Y'),
 	 ('AdditionalFacilities',NULL,'AF-VC','Video Conferencing',NULL,NULL,NULL,NULL,NULL,NULL,'Y'),
 	 ('AdditionalFacilities',NULL,'AF-VF','Video Facility',NULL,NULL,NULL,NULL,NULL,NULL,'Y'),
-	 ('AdditionalFacilities',NULL,'AF-PVL','Prison Video Link',NULL,NULL,NULL,NULL,NULL,NULL,'Y'),
+	 ('AdditionalFacilities',NULL,'AF-PVL','Prison Video Link',NULL,NULL,NULL,NULL,NULL,NULL,'Y');
+
+INSERT INTO interpreter_and_sign_language (categorykey,serviceid,"key",value_en,value_cy,hinttext_en,hinttext_cy,lov_order,parentcategory,parentkey,active) VALUES
 	 ('InterpreterLanguage',NULL,'bel-bel','Belorussian',NULL,NULL,NULL,NULL,NULL,NULL,'Y');
