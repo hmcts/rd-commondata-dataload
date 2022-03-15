81e904eb32331e805d258a9b926d530a516c7efe
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2022-03-15 13:17:34 +0000

    unit test cases

---
+++ src/test/java/uk/gov/hmcts/reform/rd/commondata/camel/binder/ListOfValuesTest.java
@@ -0,0 +1,53 @@
+package uk.gov.hmcts.reform.rd.commondata.camel.binder;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class ListOfValuesTest {
+
+    @Test
+    void testListOfValues() {
+        ListOfValues listOfValues = ListOfValues.builder().categoryKey("TEST001")
+            .serviceId("XXXX")
+            .key("TEST001")
+            .valueEN("TEST001")
+            .valueCY("TEST001")
+            .hintTextEN("TEST001")
+            .hintTextCY("TEST001")
+            .lovOrder("TEST001")
+            .parentCategory("TEST001")
+            .parentKey("TEST001")
+            .active("Y")
+            .build();
+        Assertions.assertEquals("TEST001", listOfValues.getCategoryKey());
+        Assertions.assertEquals("XXXX", listOfValues.getServiceId());
+        Assertions.assertEquals("TEST001", listOfValues.getKey());
+        Assertions.assertEquals("TEST001", listOfValues.getValueEN());
+        Assertions.assertEquals("TEST001", listOfValues.getValueCY());
+        Assertions.assertEquals("TEST001", listOfValues.getHintTextEN());
+        Assertions.assertEquals("TEST001", listOfValues.getHintTextCY());
+        Assertions.assertEquals("TEST001", listOfValues.getLovOrder());
+        Assertions.assertEquals("TEST001", listOfValues.getParentCategory());
+        Assertions.assertEquals("TEST001", listOfValues.getParentKey());
+        Assertions.assertEquals("Y", listOfValues.getActive());
+
+        String listOfValuesBuilderString = ListOfValues.builder()
+            .categoryKey("TEST002")
+            .serviceId("XXXX")
+            .key("TEST002")
+            .valueEN("TEST002")
+            .valueCY("TEST002")
+            .hintTextEN("TEST002")
+            .hintTextCY("TEST002")
+            .lovOrder("TEST002")
+            .parentCategory("TEST002")
+            .parentKey("TEST002")
+            .active("Y")
+            .toString();
+        Assertions.assertEquals(listOfValuesBuilderString, "ListOfValues.ListOfValuesBuilder("
+            + "categoryKey=TEST002, serviceId=XXXX, key=TEST002, valueEN=TEST002, valueCY=TEST002, hintTextEN=TEST002"
+            + ", hintTextCY=TEST002, lovOrder=TEST002, parentCategory=TEST002, parentKey=TEST002, active=Y)");
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/reform/rd/commondata/camel/mapper/ListOfValuesMapperTest.java
@@ -0,0 +1,56 @@
+package uk.gov.hmcts.reform.rd.commondata.camel.mapper;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.rd.commondata.camel.binder.FlagService;
+import uk.gov.hmcts.reform.rd.commondata.camel.binder.ListOfValues;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.apache.commons.lang.StringUtils.trim;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class ListOfValuesMapperTest {
+
+    @Spy
+    ListOfValuesMapper listOfValuesMapper = new ListOfValuesMapper();
+
+    @Test
+    void testMapper() {
+        ListOfValues listOfValues = ListOfValues.builder()
+            .categoryKey("TEST002")
+            .serviceId("XXXX")
+            .key("TEST002")
+            .valueEN("TEST002")
+            .valueCY("TEST002")
+            .hintTextEN("TEST002")
+            .hintTextCY("TEST002")
+            .lovOrder("002")
+            .parentCategory("TEST002")
+            .parentKey("TEST002")
+            .active("Y")
+            .build();
+        var expected = new HashMap<String, Object>();
+        expected.put("categoryKey", trim(listOfValues.getCategoryKey()));
+        expected.put("serviceId", trim(listOfValues.getServiceId()));
+        expected.put("key", trim(listOfValues.getKey()));
+        expected.put("value_en", trim(listOfValues.getValueEN()));
+        expected.put("value_cy", trim(listOfValues.getValueCY()));
+        expected.put("hinttext_en", trim(listOfValues.getHintTextEN()));
+        expected.put("hinttext_cy", trim(listOfValues.getHintTextCY()));
+        expected.put("lov_order", Long.valueOf(trim(listOfValues.getLovOrder())));
+        expected.put("parentcategory", trim(listOfValues.getParentCategory()));
+        expected.put("parentkey", trim(listOfValues.getParentKey()));
+        expected.put("active", trim(listOfValues.getActive()));
+
+        Map<String, Object> actual = listOfValuesMapper.getMap(listOfValues);
+        verify(listOfValuesMapper, times(1)).getMap(listOfValues);
+        Assertions.assertThat(actual).hasSize(11).isEqualTo(expected);
+    }
+}
