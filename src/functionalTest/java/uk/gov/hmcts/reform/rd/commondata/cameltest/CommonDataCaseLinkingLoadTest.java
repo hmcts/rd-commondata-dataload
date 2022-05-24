e0399d83740f098b3350941ea1a46aa9a375abf9
shivajilokare <shivaji.lokare@HCTS.NET>
2022-05-24 12:06:40 +0100

    updated functional test

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/cameltest/CommonDataCaseLinkingLoadTest.java
@@ -0,0 +1,211 @@
+package uk.gov.hmcts.reform.rd.commondata.cameltest;
+
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.apache.camel.test.spring.junit5.CamelTestContextBootstrapper;
+import org.apache.camel.test.spring.junit5.MockEndpoints;
+import org.javatuples.Pair;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import uk.gov.hmcts.reform.data.ingestion.camel.util.MappingConstants;
+import uk.gov.hmcts.reform.data.ingestion.configuration.AzureBlobConfig;
+import uk.gov.hmcts.reform.data.ingestion.configuration.BlobStorageCredentials;
+import uk.gov.hmcts.reform.rd.commondata.camel.binder.Categories;
+import uk.gov.hmcts.reform.rd.commondata.camel.task.CommonDataCaseLinkingRouteTask;
+import uk.gov.hmcts.reform.rd.commondata.cameltest.testsupport.CommonDataFunctionalBaseTest;
+import uk.gov.hmcts.reform.rd.commondata.cameltest.testsupport.SpringStarter;
+import uk.gov.hmcts.reform.rd.commondata.config.CommonDataCamelConfig;
+import uk.gov.hmcts.reform.rd.commondata.configuration.BatchConfig;
+
+import java.io.FileInputStream;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.util.ResourceUtils.getFile;
+import static uk.gov.hmcts.reform.data.ingestion.camel.util.MappingConstants.SCHEDULER_START_TIME;
+
+@TestPropertySource(properties = {"spring.config.location=classpath:application-functional.yml"})
+@CamelSpringBootTest
+@MockEndpoints("log:*")
+@ContextConfiguration(classes = {CommonDataCamelConfig.class, CamelTestContextBootstrapper.class,
+    JobLauncherTestUtils.class, BatchConfig.class, AzureBlobConfig.class, BlobStorageCredentials.class},
+    initializers = ConfigFileApplicationContextInitializer.class)
+@SpringBootTest
+@EnableAutoConfiguration(exclude = JpaRepositoriesAutoConfiguration.class)
+@EnableTransactionManagement
+@SqlConfig(dataSource = "dataSource", transactionManager = "txManager",
+    transactionMode = SqlConfig.TransactionMode.ISOLATED)
+@SuppressWarnings("unchecked")
+
+public class CommonDataCaseLinkingLoadTest extends CommonDataFunctionalBaseTest {
+
+    @Autowired
+    CommonDataCaseLinkingRouteTask commonDataCaseLinkingRouteTask;
+
+    @Autowired
+    @Qualifier("springJdbcTransactionManager")
+    protected PlatformTransactionManager platformTransactionManager;
+
+    private static final String CASE_LINKING_TABLE_NAME = "case_linking";
+
+    @BeforeEach
+    public void init() {
+        SpringStarter.getInstance().restart();
+        camelContext.getGlobalOptions()
+            .put(SCHEDULER_START_TIME, String.valueOf(new Date(System.currentTimeMillis()).getTime()));
+    }
+
+    @Test
+    @DisplayName("Status: Success - Test for loading a valid Csv file in to a clean list_of_values table")
+    public void testListOfValuesCsv_Success() throws Exception {
+        testListOfValuesInsertion(
+            "case_linking_success.csv",
+            MappingConstants.SUCCESS
+        );
+    }
+
+    @Test
+    @DisplayName("Status: Success - Test for loading a valid Csv file with different headers in different cases "
+        + "in to a clean list_of_values table")
+    public void testListOfValuesCsv_TestCaseInsensitiveHeaders_Success() throws Exception {
+        String fileName = "case_linking_success_insensitive_headers.csv";
+        testListOfValuesInsertion(
+            fileName,
+            MappingConstants.SUCCESS
+        );
+    }
+
+    @Test
+    @DisplayName("Status: Success - Test for loading a valid Csv file which has headers and data enclosed within quotes"
+        + " in to a clean list_of_values table")
+    public void testListOfValuesCsv_WithQuotes_Success() throws Exception {
+        String fileName = "case_linking_success_with_quotes.csv";
+        testListOfValuesInsertion(
+            fileName,
+            MappingConstants.SUCCESS
+        );
+    }
+
+    @Test
+    @DisplayName("Status: Success - Test for loading a valid Csv file which has a leading "
+        + "trailing white space")
+    @Sql(scripts = {"/testData/commondata_truncate.sql"})
+    void testListOfValuesCsv_WithLeadingTrailingSpace_Success() throws Exception {
+        commonDataBlobSupport.uploadFile(
+            UPLOAD_LIST_OF_VALUES_FILE_NAME,
+            new FileInputStream(getFile(
+                "classpath:sourceFiles/categories/list_of_values_success_leadingspace.csv"))
+        );
+
+        jobLauncherTestUtils.launchJob();
+        //Validate Success Result
+        validateListOfValuesFile(jdbcTemplate, listOfValuesSelectData, List.of(
+            Categories.builder().categoryKey("AdditionalFacilities").serviceId("").key("AF-VF")
+                .valueEN("Video Facility").valueCY("").hintTextEN("").hintTextCY("").parentCategory("")
+                .parentKey("").active("Y").build()), 1);
+        //Validates Success Audit
+        validateFlagServiceFileAudit(jdbcTemplate, auditSchedulerQuery, "Success", UPLOAD_LIST_OF_VALUES_FILE_NAME);
+    }
+
+    @Test
+    @DisplayName("Status: Success - Test for loading a valid Csv file which has a  numeric"
+        + "value for CategoryKey field")
+    @Sql(scripts = {"/testData/commondata_truncate.sql"})
+    public void testListOfValuesCsv_WithNonNumericValueCategoryKey_Success() throws Exception {
+        commonDataBlobSupport.uploadFile(
+            UPLOAD_CASE_LINKING_FILE_NAME,
+            new FileInputStream(getFile(
+                "classpath:sourceFiles/caseLinking/case_linking_partial_success_categoryKey_numeric.csv"))
+        );
+
+        jobLauncherTestUtils.launchJob();
+        validateListOfValuesFile(jdbcTemplate, listOfValuesSelectData, List.of(
+            Categories.builder().categoryKey("1").serviceId("").key("AF-WR")
+                .valueEN("Witness Room").valueCY("").hintTextEN("").hintTextCY("").parentCategory("")
+                .parentKey("").active("Y").build()), 1);
+        //Validates Success Audit
+        validateFlagServiceFileAudit(jdbcTemplate, auditSchedulerQuery, "Success", UPLOAD_CASE_LINKING_FILE_NAME);
+    }
+
+    @Test
+    @DisplayName("Status: Failure - Test for loading a file with an additional unknown header.")
+    @Sql(scripts = {"/testData/commondata_truncate.sql"})
+    void testListOfValuesCsv_UnknownHeader_Failure() throws Exception {
+        commonDataBlobSupport.uploadFile(
+            UPLOAD_CASE_LINKING_FILE_NAME,
+            new FileInputStream(getFile(
+                "classpath:sourceFiles/caseLinking/case_linking_failure_additional_header.csv"))
+        );
+
+        jobLauncherTestUtils.launchJob();
+        var listOfValues = jdbcTemplate.queryForList(listOfValuesSelectData);
+        assertEquals(0, listOfValues.size());
+
+        Pair<String, String> pair = new Pair<>(
+            UPLOAD_CASE_LINKING_FILE_NAME,
+            "There is a mismatch in the headers of the csv file :: ListOfValues-test.csv"
+        );
+        validateFlagServiceFileException(jdbcTemplate, exceptionQuery, pair, 1);
+        validateFlagServiceFileAudit(jdbcTemplate, auditSchedulerQuery, "Failure", UPLOAD_CASE_LINKING_FILE_NAME);
+    }
+
+    @Test
+    @DisplayName("Status: Failure - Test for loading a file with a missing header.")
+    @Sql(scripts = {"/testData/commondata_truncate.sql"})
+    void testListOfValuesCsv_MissingHeader_Failure() throws Exception {
+        commonDataBlobSupport.uploadFile(
+            UPLOAD_CASE_LINKING_FILE_NAME,
+            new FileInputStream(getFile(
+                "classpath:sourceFiles/caseLinking/case_linking_failure_unknown_header.csv"))
+        );
+
+        jobLauncherTestUtils.launchJob();
+        var listOfValues = jdbcTemplate.queryForList(listOfValuesSelectData);
+        assertEquals(0, listOfValues.size());
+
+        Pair<String, String> pair = new Pair<>(
+            UPLOAD_CASE_LINKING_FILE_NAME,
+            "There is a mismatch in the headers of the csv file :: ListOfValues-test.csv"
+        );
+        validateFlagServiceFileException(jdbcTemplate, exceptionQuery, pair, 1);
+        validateFlagServiceFileAudit(jdbcTemplate, auditSchedulerQuery, "Failure", UPLOAD_CASE_LINKING_FILE_NAME);
+    }
+
+    private void testListOfValuesInsertion(String fileName, String status) throws Exception {
+        commonDataBlobSupport.uploadFile(
+            UPLOAD_CASE_LINKING_FILE_NAME,
+            new FileInputStream(getFile(String.format("classpath:sourceFiles/caseLinking/%s", fileName)))
+        );
+
+        camelContext.getGlobalOptions()
+            .put(SCHEDULER_START_TIME, String.valueOf(new Date(System.currentTimeMillis()).getTime()));
+        jobLauncherTestUtils.launchJob();
+
+    }
+
+
+
+    @AfterEach
+    void tearDown() throws Exception {
+        //Delete Uploaded test file with Snapshot delete
+        commonDataBlobSupport.deleteBlob(UPLOAD_CASE_LINKING_FILE_NAME);
+    }
+
+
+
+}
