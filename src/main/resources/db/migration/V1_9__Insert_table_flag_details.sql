80d1e4dbbb8c8e8900f089b648c5d11b7cfd2c81
amitkumarshrivastava <86308925+amitkumarshrivastava@users.noreply.github.com>
2022-06-22 12:56:56 +0100

    RDCC-4906 CRD : Test Flag Details Changes (#121)
    * RDCC-4906 CRD : Test Flag Details Changes
    
    * Update V1_9__Insert_table_flag_details.sql

---
+++ src/main/resources/db/migration/V1_9__Insert_table_flag_details.sql
@@ -0,0 +1,2 @@
+INSERT INTO flag_details (id, flag_code, value_en, value_cy, category_id, mrd_created_time, mrd_updated_time, mrd_deleted_time) VALUES
+    (83, 'CF0014', 'Welsh forms and communications', '', 1, '2022-06-17 13:33:00', '2022-06-17 13:33:00', NULL);
