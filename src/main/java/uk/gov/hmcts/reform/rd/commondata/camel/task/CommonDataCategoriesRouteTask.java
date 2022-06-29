719505cf20a7693b9c5327e59f3376efb6b43299
shivajilokare <shivaji.lokare@HCTS.NET>
2022-06-29 09:40:22 +0100

    fix

--- src/main/java/uk/gov/hmcts/reform/rd/commondata/camel/task/CommonDataCategoriesRouteTask.java
+++ src/main/java/uk/gov/hmcts/reform/rd/commondata/camel/task/CommonDataCategoriesRouteTask.java
@@ -22,8 +22,7 @@ public class CommonDataCategoriesRouteTask extends BaseTasklet implements Taskle
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
-        return super.execute(startRoute, routesToExecute, Boolean.TRUE);
-        //master build
+        return super.execute(startRoute, routesToExecute, Boolean.FALSE);
     }
 
 }
