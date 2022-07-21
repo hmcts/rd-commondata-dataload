1df9c75c081bc488c245ca5c82d01e766d0cb106
shivajihmcts <89258035+shivajihmcts@users.noreply.github.com>
2022-07-21 08:19:30 +0100

    Feat:RDCC-4997:Changes made to  add and fix checkstyle issues (#136)
    * RDCC-4997:Changes made to  add and fix checkstyle issues
    
    * RDCC-4997:Changes made to  add and fix fortifyscan issues

--- src/main/java/uk/gov/hmcts/reform/rd/commondata/camel/processor/CategoriesProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/rd/commondata/camel/processor/CategoriesProcessor.java
@@ -1,9 +1,8 @@
 package uk.gov.hmcts.reform.rd.commondata.camel.processor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Exchange;
 import org.springframework.stereotype.Component;
-
-import lombok.extern.slf4j.Slf4j;
 import uk.gov.hmcts.reform.data.ingestion.camel.processor.JsrValidationBaseProcessor;
 import uk.gov.hmcts.reform.rd.commondata.camel.binder.Categories;
 
