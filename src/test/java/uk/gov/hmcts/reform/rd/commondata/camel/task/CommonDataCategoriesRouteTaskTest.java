eab5c2fc691f5560c79fd282c2ab1f507b4377f1
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2022-08-01 09:29:34 +0100

    Audit Active D flag Records

--- src/test/java/uk/gov/hmcts/reform/rd/commondata/camel/task/CommonDataCategoriesRouteTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/rd/commondata/camel/task/CommonDataCategoriesRouteTaskTest.java
@@ -1,19 +1,27 @@
 package uk.gov.hmcts.reform.rd.commondata.camel.task;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import uk.gov.hmcts.reform.data.ingestion.camel.route.DataLoadRoute;
+import uk.gov.hmcts.reform.data.ingestion.camel.validator.JsrValidatorInitializer;
+import uk.gov.hmcts.reform.rd.commondata.camel.binder.Categories;
 import uk.gov.hmcts.reform.rd.commondata.camel.util.CommonDataExecutor;
 
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.isNull;
@@ -35,23 +43,40 @@ class CommonDataCategoriesRouteTaskTest {
 
     CamelContext camelContext = new DefaultCamelContext();
 
+    JdbcTemplate jdbcTemplate = mock(JdbcTemplate.class);
+
+    @Mock
+    JsrValidatorInitializer<Categories> categoriesJsrValidatorInitializer;
+
     @BeforeEach
     void init() {
         setField(commonDataCategoriesRouteTask, "logComponentName", "testlogger");
         setField(commonDataCategoriesRouteTask, "dataLoadRoute", dataLoadRoute);
         setField(commonDataCategoriesRouteTask, "commonDataExecutor", commonDataExecutor);
         setField(commonDataCategoriesRouteTask, "camelContext", camelContext);
+        setField(commonDataCategoriesRouteTask, "jdbcTemplate", jdbcTemplate);
+        setField(commonDataCategoriesRouteTask, "categoriesJsrValidatorInitializer",
+                 categoriesJsrValidatorInitializer);
     }
 
     @Test
     void testExecute() {
         doNothing().when(dataLoadRoute).startRoute(anyString(), anyList());
         when(commonDataExecutor.execute(any(), anyString(), anyString())).thenReturn("success");
-        Assertions.assertEquals(RepeatStatus.FINISHED, commonDataCategoriesRouteTask
-            .execute(anyString(), anyList(), anyBoolean()));
-        verify(dataLoadRoute,times(1)).startRoute(anyString(),anyList());
-        verify(commonDataCategoriesRouteTask, times(1))
-            .execute(anyString(), anyList(), anyBoolean());
+        when(jdbcTemplate.query(anyString(), ArgumentMatchers.<RowMapper<Categories>>any(), anyString()))
+            .thenReturn(List.of(
+                Categories.builder().categoryKey("categoryKey").serviceId("serviceId").key("key").build()
+            ));
+        when(jdbcTemplate.update(anyString(), anyString())).thenReturn(1);
+
+        Assertions.assertEquals(RepeatStatus.FINISHED, commonDataCategoriesRouteTask.execute(any(), any()));
+        verify(dataLoadRoute,times(1)).startRoute(any(),any());
+        verify(commonDataCategoriesRouteTask, times(1)).execute(any(), any());
+        verify(categoriesJsrValidatorInitializer,times(1))
+            .auditJsrExceptions(ArgumentMatchers.any(), any(), anyString(), any(Exchange.class));
+        verify(jdbcTemplate, times(1))
+            .query(anyString(), ArgumentMatchers.<RowMapper<Categories>>any(), anyString());
+        verify(jdbcTemplate, times(1)).update(anyString(), anyString());
     }
 
     @Test
