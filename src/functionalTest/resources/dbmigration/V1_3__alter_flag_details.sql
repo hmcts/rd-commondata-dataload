e2ccb9cfe3fb0e4bef4678a6c476defa31449a56
shivajihmcts <89258035+shivajihmcts@users.noreply.github.com>
2022-08-04 15:08:33 +0100

    RDCC-5178:Changes made to sync flywayscripts (#148)

---
+++ src/functionalTest/resources/dbmigration/V1_3__alter_flag_details.sql
@@ -0,0 +1,3 @@
+ALTER TABLE flag_details ADD COLUMN mrd_created_time TIMESTAMP;
+ALTER TABLE flag_details ADD COLUMN mrd_updated_time TIMESTAMP;
+ALTER TABLE flag_details ADD COLUMN mrd_deleted_time TIMESTAMP;
