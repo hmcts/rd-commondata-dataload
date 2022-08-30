460d730509ffadb5e9d660b26ac54c24165de9f4
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2022-08-30 14:33:00 +0100

    Insert new flag details value

---
+++ src/main/resources/db/migration/V2_0__Insert_table_flag_details.sql
@@ -0,0 +1,2 @@
+INSERT INTO flag_details (id, flag_code, value_en, value_cy, category_id, mrd_created_time, mrd_updated_time, mrd_deleted_time) VALUES
+    (84, 'PF0020', 'QLR (Qualified Legal Representative)', '', 2, '18-08-2022 13:03:00', '18-08-2022 13:03:00', NULL);
