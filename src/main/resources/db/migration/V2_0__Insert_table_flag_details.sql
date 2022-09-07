811ebaf98bb6372bebb1c7313e8f6986f009153b
sahitya-desireddy <93707379+sahitya-desireddy@users.noreply.github.com>
2022-09-07 14:46:51 +0100

    Rdcc 5328 (#157)
    * Audit Active D flag Records
    
    * Audit Active D flag Records tests
    
    * Audit Active D flag Records functional tests
    
    * Log Message modified
    
    * Log Message modified
    
    * Renaming JsrValidatorInitializer in test cases
    
    * Renaming JsrValidatorInitializer in test cases
    
    * Insert new flag details value
    
    * Changing Date Format

---
+++ src/main/resources/db/migration/V2_0__Insert_table_flag_details.sql
@@ -0,0 +1,2 @@
+INSERT INTO flag_details (id, flag_code, value_en, value_cy, category_id, mrd_created_time, mrd_updated_time, mrd_deleted_time) VALUES
+    (84, 'PF0020', 'QLR (Qualified Legal Representative)', '', 2, '2022-08-18 13:03:00', '2022-08-18 13:03:00', NULL);
