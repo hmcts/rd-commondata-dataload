aba1cc267b538db85537f154d2bfa345386cce9c
nayeemshaik-hmcts <nayeem.shaik@hmcts.net>
2022-11-03 16:49:31 +0000

    RDCC-5741 -updated flag details

--- src/main/resources/db/migration/V1_10__update_table_flag_details.sql
+++ src/main/resources/db/migration/V1_10__update_table_flag_details.sql
@@ -1,3 +1,4 @@
-UPDATE flag_details SET value_en = 'Explanation of the court or tribunal and who''s in the room at the hearing'
-      WHERE flag_code='RA0047' AND  category_id='11';
+DELETE FROM flag_details where id = 50 and flag_code = 'RA0047' and category_id=11;
+INSERT INTO flag_details (id, flag_code, value_en, value_cy, category_id, mrd_created_time, mrd_updated_time, mrd_deleted_time) VALUES
+    (85, 'RA0047', 'Explanation of the court or tribunal and who''s in the room at the hearing', '', 11, '2022-10-27 12:33', '2022-10-27 12:33', NULL);
 COMMIT;
