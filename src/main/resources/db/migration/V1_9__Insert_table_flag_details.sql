20a12f6a9ff36adabcc470f5f9860440f57bc444
nayeemshaik-hmcts <nayeem.shaik@hmcts.net>
2022-11-09 15:50:38 +0000

    RDCC-5741 -updated flag details

--- src/main/resources/db/migration/V1_9__Insert_table_flag_details.sql
+++ src/main/resources/db/migration/V1_9__Insert_table_flag_details.sql
@@ -1,2 +1,12 @@
-INSERT INTO flag_details (id, flag_code, value_en, value_cy, category_id, mrd_created_time, mrd_updated_time, mrd_deleted_time) VALUES
-    (83, 'CF0014', 'Welsh forms and communications', '', 1, '2022-06-17 13:33:00', '2022-06-17 13:33:00', NULL);
+--Inserting values for testing
+INSERT INTO flag_details (id, flag_code, value_en, value_cy, category_id, mrd_created_time, mrd_updated_time, mrd_deleted_time)
+ VALUES
+    (84,'PF0020', 'QLR (Qualified Legal Representative)', '', 2, '2022-08-18 13:03:00', '2022-08-18 13:03:00', NULL);
+
+insert into flag_service(id, service_id, hearing_relevant, request_reason, flag_code) values (1, 'XXXX', false, false, 'RA0004');
+insert into flag_service(id, service_id, hearing_relevant, request_reason, flag_code) values (2, 'XXXX', false, false, 'RA0042');
+insert into flag_service(id, service_id, hearing_relevant, request_reason, flag_code) values (3, 'XXXX', false, false, 'PF0015');
+insert into flag_service(id, service_id, hearing_relevant, request_reason, flag_code) values (4,	'AAA1',	true,	true,	'CF0002');
+insert into flag_service(id, service_id, hearing_relevant, request_reason, flag_code) values (5,	'AAA1',	true,	false, 'PF0020');
+
+
--- src/main/resources/db/migration/V2_1__update_table_flag_details.sql
+++
@@ -1,3 +0,0 @@
-UPDATE flag_details SET value_en = 'Explanation of the court or tribunal and who''s in the room at the hearing'
-      WHERE flag_code='RA0047' AND  category_id='11';
-COMMIT;
