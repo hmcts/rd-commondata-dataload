a4a1f1f33149c86f7dc932bca561b8e020bb21c5
ishratt <tunazzina.ishrat@hmcts.net>
2022-12-21 09:23:41 +0000

    minor functional tests changes

--- src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/cameltest/CommonDataFlagDetailsLoadTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/cameltest/CommonDataFlagDetailsLoadTest.java
@@ -4,6 +4,7 @@ import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.apache.camel.test.spring.junit5.CamelTestContextBootstrapper;
 import org.apache.camel.test.spring.junit5.MockEndpoints;
 import org.javatuples.Pair;
+import org.javatuples.Quartet;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -37,6 +38,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -66,6 +68,8 @@ public class CommonDataFlagDetailsLoadTest extends CommonDataFunctionalBaseTest 
     @Autowired
     @Qualifier("springJdbcTransactionManager")
     protected PlatformTransactionManager platformTransactionManager;
+
+    private static final String FLAG_DETAILS_TABLE_NAME = "flag_details";
 
     private static final String HEADER_MISMATCH_MESSAGE
         = "There is a mismatch in the headers of the csv file :: FlagDetails-test.csv";
@@ -150,7 +154,7 @@ public class CommonDataFlagDetailsLoadTest extends CommonDataFunctionalBaseTest 
             UPLOAD_FLAG_DETAILS_FILE_NAME,
             HEADER_MISMATCH_MESSAGE
         );
-        validateFlagDetailsFileException(jdbcTemplate, orderedExceptionQuery, pair, 3);
+        validateFlagDetailsFileFailureException(jdbcTemplate, orderedExceptionQuery, pair, 3);
         validateFileAudit(jdbcTemplate, auditSchedulerQuery, FAILURE_MESSAGE, UPLOAD_FLAG_DETAILS_FILE_NAME);
     }
 
@@ -167,18 +171,18 @@ public class CommonDataFlagDetailsLoadTest extends CommonDataFunctionalBaseTest 
             .mrdCreatedTime("2022-04-07 12:43:00").mrdUpdatedTime("2022-06-17 13:33:00").build();
     }
 
+    @SuppressWarnings("unchecked")
     @Test
-    @DisplayName("Status: PartialSuccess - Test for loading a valid Csv file which has a combination of "
-        + "valid entries and entries missing a mandatory field")
+    @DisplayName("Status: PartialSuccess - Test for loading a valid Csv file which has an entry with missing flag code")
     @Sql(scripts = {"/testData/commondata_truncate.sql"})
-    public void testFlagDetailsCsv_WithMissingMandatoryValue_PartialSuccess() throws Exception {
-        String fileName = "flag_details_partial_success.csv";
+    public void testFlagDetailsCsv_WithMissingFlagCode_PartialSuccess() throws Exception {
+        String fileName = "flag_details_partial_success_missing_flag_code.csv";
         testFlagDetailsInsertion(
             fileName,
             MappingConstants.PARTIAL_SUCCESS
         );
         var result = jdbcTemplate.queryForList(exceptionRecordsQuery);
-        assertEquals(4, result.size());
+        assertEquals(1, result.size());
 
         validateFileAudit(
             jdbcTemplate,
@@ -186,8 +190,85 @@ public class CommonDataFlagDetailsLoadTest extends CommonDataFunctionalBaseTest 
             MappingConstants.PARTIAL_SUCCESS,
             UPLOAD_FLAG_DETAILS_FILE_NAME
         );
+
+        Quartet<String, String, String, Long> quartet = Quartet.with("flagCode","Flag Code is missing","3",4L);
+        validateFlagDetailsFileJsrException(jdbcTemplate, orderedExceptionQuery, quartet);
     }
 
+    @SuppressWarnings("unchecked")
+    @Test
+    @DisplayName("Status: PartialSuccess - Test for loading a valid Csv file which has an entry with missing value_en")
+    @Sql(scripts = {"/testData/commondata_truncate.sql"})
+    public void testFlagDetailsCsv_WithMissingValueEn_PartialSuccess() throws Exception {
+        String fileName = "flag_details_partial_success_missing_value_en.csv";
+        testFlagDetailsInsertion(
+            fileName,
+            MappingConstants.PARTIAL_SUCCESS
+        );
+        var result = jdbcTemplate.queryForList(exceptionRecordsQuery);
+        assertEquals(1, result.size());
+
+        validateFileAudit(
+            jdbcTemplate,
+            auditSchedulerQuery,
+            MappingConstants.PARTIAL_SUCCESS,
+            UPLOAD_FLAG_DETAILS_FILE_NAME
+        );
+
+        Quartet<String, String, String, Long> quartet = Quartet.with("valueEn","Value_EN is missing","3",4L);
+        validateFlagDetailsFileJsrException(jdbcTemplate, orderedExceptionQuery, quartet);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    @DisplayName("Status: PartialSuccess - Test for loading a valid Csv file which has an entry with missing category "
+        + "Id")
+    @Sql(scripts = {"/testData/commondata_truncate.sql"})
+    public void testFlagDetailsCsv_WithMissingCategoryId_PartialSuccess() throws Exception {
+        String fileName = "flag_details_partial_success_missing_categoryId.csv";
+        testFlagDetailsInsertion(
+            fileName,
+            MappingConstants.PARTIAL_SUCCESS
+        );
+        var result = jdbcTemplate.queryForList(exceptionRecordsQuery);
+        assertEquals(1, result.size());
+
+        validateFileAudit(
+            jdbcTemplate,
+            auditSchedulerQuery,
+            MappingConstants.PARTIAL_SUCCESS,
+            UPLOAD_FLAG_DETAILS_FILE_NAME
+        );
+
+        Quartet<String, String, String, Long> quartet = Quartet.with("categoryId","Category_ID is missing","3",4L);
+        validateFlagDetailsFileJsrException(jdbcTemplate, orderedExceptionQuery, quartet);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    @DisplayName("Status: PartialSuccess - Test for loading a valid Csv file which has an entry with missing id")
+    @Sql(scripts = {"/testData/commondata_truncate.sql"})
+    public void testFlagDetailsCsv_WithMissingId_PartialSuccess() throws Exception {
+        String fileName = "flag_details_partial_success_missing_id.csv";
+        testFlagDetailsInsertion(
+            fileName,
+            MappingConstants.PARTIAL_SUCCESS
+        );
+        var result = jdbcTemplate.queryForList(exceptionRecordsQuery);
+        assertEquals(1, result.size());
+
+        validateFileAudit(
+            jdbcTemplate,
+            auditSchedulerQuery,
+            MappingConstants.PARTIAL_SUCCESS,
+            UPLOAD_FLAG_DETAILS_FILE_NAME
+        );
+
+        Quartet<String, String, String, Long> quartet = Quartet.with("id","ID is missing","",4L);
+        validateFlagDetailsFileJsrException(jdbcTemplate, orderedExceptionQuery, quartet);
+    }
+
+    @SuppressWarnings("unchecked")
     @Test
     @DisplayName("Status: PartialSuccess - Test for loading a valid Csv file which has expired records")
     @Sql(scripts = {"/testData/commondata_truncate.sql"})
@@ -213,6 +294,9 @@ public class CommonDataFlagDetailsLoadTest extends CommonDataFunctionalBaseTest 
             FlagDetails.builder().id("2").flagCode("PF0001").valueEn("Party").valueCy("").categoryId("0")
                 .mrdCreatedTime("2022-04-07 12:43:00").mrdUpdatedTime("2022-06-17 13:33:00").build()
         ));
+
+        Quartet<String, String, String, Long> quartet = Quartet.with("", "Record is expired", "PF0001", 4L);
+        validateFlagDetailsFileJsrException(jdbcTemplate, exceptionQuery, quartet);
     }
 
     @Test
@@ -232,7 +316,7 @@ public class CommonDataFlagDetailsLoadTest extends CommonDataFunctionalBaseTest 
             UPLOAD_FLAG_DETAILS_FILE_NAME,
             "No records have been defined in the CSV"
         );
-        validateFlagDetailsFileException(jdbcTemplate, orderedExceptionQuery, pair, 3);
+        validateFlagDetailsFileFailureException(jdbcTemplate, orderedExceptionQuery, pair, 3);
         validateFileAudit(jdbcTemplate, auditSchedulerQuery, FAILURE_MESSAGE, UPLOAD_FLAG_DETAILS_FILE_NAME);
     }
 
@@ -245,13 +329,30 @@ public class CommonDataFlagDetailsLoadTest extends CommonDataFunctionalBaseTest 
         auditEntry.ifPresent(audit -> assertEquals(status, audit.get("status")));
     }
 
-    protected void validateFlagDetailsFileException(JdbcTemplate jdbcTemplate,
-                                                    String exceptionQuery,
-                                                    Pair<String, String> pair,
-                                                    int index) {
+    protected void validateFlagDetailsFileFailureException(JdbcTemplate jdbcTemplate, String exceptionQuery,
+                                                           Pair<String, String> pair, int index) {
         var result = jdbcTemplate.queryForList(exceptionQuery);
         assertThat((String) result.get(index).get("error_description"),
                    containsString(pair.getValue1()));
+    }
+
+    @SuppressWarnings("unchecked")
+    protected void validateFlagDetailsFileJsrException(JdbcTemplate jdbcTemplate, String exceptionQuery,
+                                                       Quartet<String, String, String, Long>... quartets) {
+        var result = jdbcTemplate.queryForList(exceptionQuery);
+
+        List<Map<String, Object>> actualResult = result.stream()
+            .filter(exception -> exception.containsValue(CommonDataFlagDetailsLoadTest.FLAG_DETAILS_TABLE_NAME))
+            .collect(Collectors.toUnmodifiableList());
+        for (Map<String, Object> currResult : actualResult) {
+            for (Quartet<String, String, String, Long> quartet : quartets) {
+                if (quartet.getValue1().equals(currResult.get("error_description"))) {
+                    assertEquals(quartet.getValue0(), currResult.get("field_in_error"));
+                    assertEquals(quartet.getValue2(), currResult.get("key"));
+                    assertEquals(quartet.getValue3(), currResult.get("row_id"));
+                }
+            }
+        }
     }
 
     private void testFlagDetailsInsertion(String fileName, String status) throws Exception {
