17b10ab971a692c41a2cb27edb595d3265aa2b7d
Lukasz Wolski <1005015+lukasz-wolski@users.noreply.github.com>
2023-04-28 09:15:57 +0100

    added dependencies to jakarta.el

--- src/test/java/uk/gov/hmcts/reform/rd/commondata/camel/binder/FlagDetailsTest.java
+++ src/test/java/uk/gov/hmcts/reform/rd/commondata/camel/binder/FlagDetailsTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 
 class FlagDetailsTest {
 
