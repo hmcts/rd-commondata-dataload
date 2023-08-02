72def7c451558946b8218c7bcd9488d41bfc3688
Lukasz Wolski <1005015+lukasz-wolski@users.noreply.github.com>
2023-08-02 11:59:19 +0100

    apache-camel 4.0.0-RC2

--- src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/cameltest/testsupport/CommonDataBlobSupport.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/cameltest/testsupport/CommonDataBlobSupport.java
@@ -1,12 +1,12 @@
 package uk.gov.hmcts.reform.rd.commondata.cameltest.testsupport;
 
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.blob.CloudBlobClient;
-import com.microsoft.azure.storage.blob.CloudBlobContainer;
-import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.common.StorageSharedKeyCredential;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.stereotype.Component;
@@ -18,7 +18,7 @@ import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static com.microsoft.azure.storage.blob.DeleteSnapshotsOption.INCLUDE_SNAPSHOTS;
+import static com.azure.storage.blob.models.DeleteSnapshotsOptionType.INCLUDE;
 import static java.util.Objects.isNull;
 
 @Component
@@ -26,16 +26,17 @@ import static java.util.Objects.isNull;
     AzureBlobConfig.class, BlobStorageCredentials.class}, initializers = ConfigDataApplicationContextInitializer.class)
 public class CommonDataBlobSupport {
 
+    @Autowired
+    AzureBlobConfig azureBlobConfig;
 
     @Autowired
-    @Qualifier("credscloudStorageAccount")
-    CloudStorageAccount acc;
+    BlobServiceClientBuilder blobServiceClientBuilder;
 
-    CloudBlobClient cloudBlobClient;
+    BlobServiceClient blobServiceClient;
 
-    CloudBlobContainer cloudBlobContainer;
+    BlobContainerClient cloudBlobContainer;
 
-    CloudBlobContainer cloudBlobArchContainer;
+    BlobContainerClient cloudBlobArchContainer;
 
     @Value("${archival-date-format}")
     private String archivalDateFormat;
@@ -47,9 +48,16 @@ public class CommonDataBlobSupport {
      */
     @PostConstruct
     public void init() throws Exception {
-        cloudBlobClient = acc.createCloudBlobClient();
-        cloudBlobContainer = cloudBlobClient.getContainerReference("rd-common-data");
-        cloudBlobArchContainer = cloudBlobClient.getContainerReference("rd-common-data-archive");
+        StorageSharedKeyCredential credential = new StorageSharedKeyCredential(
+            azureBlobConfig.getAccountName(), azureBlobConfig.getAccountKey());
+        String uri = String.format("https://%s.blob.core.windows.net", azureBlobConfig.getAccountName());
+
+        blobServiceClient = blobServiceClientBuilder
+            .endpoint(uri)
+            .credential(credential)
+            .buildClient();
+        cloudBlobContainer = blobServiceClient.createBlobContainer("rd-common-data");
+        cloudBlobArchContainer = blobServiceClient.createBlobContainer("rd-common-data-archive");
     }
 
     /**
@@ -60,7 +68,7 @@ public class CommonDataBlobSupport {
      * @throws Exception Exception
      */
     public void uploadFile(String blob, InputStream sourceFile) throws Exception {
-        CloudBlockBlob cloudBlockBlob = cloudBlobContainer.getBlockBlobReference(blob);
+        BlobClient cloudBlockBlob = cloudBlobContainer.getBlobClient(blob);
         cloudBlockBlob.upload(sourceFile, 8 * 1024 * 1024);
     }
 
@@ -73,16 +81,16 @@ public class CommonDataBlobSupport {
      */
     public void deleteBlob(String blob, boolean... status) throws Exception {
         Thread.sleep(1000);
-        CloudBlockBlob cloudBlockBlob = cloudBlobContainer.getBlockBlobReference(blob);
+        BlobClient cloudBlockBlob = cloudBlobContainer.getBlobClient(blob);
         if (cloudBlockBlob.exists()) {
-            cloudBlockBlob.delete(INCLUDE_SNAPSHOTS, null, null, null);
+            cloudBlockBlob.deleteWithResponse(INCLUDE, null, null, null);
             String date = new SimpleDateFormat(archivalDateFormat).format(new Date());
 
             //Skipped for Stale non existing files as not archived
             if (isNull(status)) {
-                cloudBlockBlob = cloudBlobArchContainer.getBlockBlobReference(blob.concat(date));
+                cloudBlockBlob = cloudBlobArchContainer.getBlobClient(blob.concat(date));
                 if (cloudBlockBlob.exists()) {
-                    cloudBlockBlob.delete(INCLUDE_SNAPSHOTS, null, null, null);
+                    cloudBlockBlob.deleteWithResponse(INCLUDE, null, null, null);
                 }
             }
         }
@@ -96,7 +104,7 @@ public class CommonDataBlobSupport {
      * @throws Exception Exception
      */
     public boolean isBlobPresent(String blob) throws Exception {
-        CloudBlockBlob cloudBlockBlob = cloudBlobContainer.getBlockBlobReference(blob);
+        BlobClient cloudBlockBlob = cloudBlobContainer.getBlobClient(blob);
         return cloudBlockBlob.exists();
     }
 }
