5781a312450ea1f40f5e9384f6adb405ede7a3af
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-10-04 12:09:56 +0100

    functional test fix

--- src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/cameltest/testsupport/CommonDataFunctionalBaseTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/cameltest/testsupport/CommonDataFunctionalBaseTest.java
@@ -186,7 +186,7 @@ public abstract class CommonDataFunctionalBaseTest {
     protected void validateFlagServiceFileAudit(JdbcTemplate jdbcTemplate,
                                                 String auditSchedulerQuery, String status, String fileName) {
         var result = jdbcTemplate.queryForList(auditSchedulerQuery);
-        assertEquals(4, result.size());
+        assertEquals(5, result.size());
         Optional<Map<String, Object>> auditEntry =
             result.stream().filter(audit -> audit.containsValue(fileName)).findFirst();
         assertTrue(auditEntry.isPresent());
