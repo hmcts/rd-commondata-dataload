3bf0d15f8216e83f4fa226f1137d0ca260a43ce3
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-10-04 10:56:38 +0100

    functional test fix

--- src/main/java/uk/gov/hmcts/reform/rd/commondata/configuration/BatchConfig.java
+++ src/main/java/uk/gov/hmcts/reform/rd/commondata/configuration/BatchConfig.java
@@ -120,11 +120,10 @@ public class BatchConfig {
             .listener(jobResultListener)
             .on("*").to(stepCommonDataRoute())
             .on("*").to(stepCommonDataCategoriesRoute())
-            .on("*").to(checkCaseLinkingRouteStatus())
-            .from(checkCaseLinkingRouteStatus()).on("STOPPED").to(stepOtherCategoriesRoute())
-            .from(checkCaseLinkingRouteStatus()).on("ENABLED").to(stepCommonDataCaseLinkingRoute())
             .on("*").to(stepOtherCategoriesRoute())
-            .end()
+            .on("*").to(checkCaseLinkingRouteStatus())
+            .from(checkCaseLinkingRouteStatus()).on("STOPPED").end()
+            .from(checkCaseLinkingRouteStatus()).on("ENABLED").to(stepCommonDataCaseLinkingRoute()).end()
             .build();
     }
 
