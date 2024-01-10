089a17a9b02e908dcd105f14b8f2f8317809078c
andy-m-b <141227049+andy-m-b@users.noreply.github.com>
2024-01-10 12:31:50 +0000

    DTSRD-1933
    - Moving hidden character check to external config.

--- src/main/java/uk/gov/hmcts/reform/rd/commondata/configuration/DataQualityCheckConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/rd/commondata/configuration/DataQualityCheckConfiguration.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.rd.commondata.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
+@ConfigurationProperties
 public class DataQualityCheckConfiguration {
 
     @Value("${zero-byte-characters}")
