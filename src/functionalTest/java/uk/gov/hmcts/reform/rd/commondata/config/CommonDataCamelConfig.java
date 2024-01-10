29424fc36e393a1ac66fa1b43199f74f8bffa585
andy-m-b <141227049+andy-m-b@users.noreply.github.com>
2024-01-10 12:49:25 +0000

    DTSRD-1933
    - Adding DataQualityCheckConfiguration bean.

--- src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/config/CommonDataCamelConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/config/CommonDataCamelConfig.java
@@ -48,6 +48,7 @@ import uk.gov.hmcts.reform.rd.commondata.camel.task.CommonDataOtherCategoriesRou
 import uk.gov.hmcts.reform.rd.commondata.camel.util.CommonDataDRecords;
 import uk.gov.hmcts.reform.rd.commondata.camel.util.CommonDataExecutor;
 import uk.gov.hmcts.reform.rd.commondata.cameltest.testsupport.CommonDataBlobSupport;
+import uk.gov.hmcts.reform.rd.commondata.configuration.DataQualityCheckConfiguration;
 
 import javax.sql.DataSource;
 
@@ -83,6 +84,11 @@ public class CommonDataCamelConfig {
     @Bean
     public CategoriesProcessor listOfValuesProcessor() {
         return new CategoriesProcessor();
+    }
+
+    @Bean
+    public DataQualityCheckConfiguration dataQualityCheckConfiguration() {
+        return new DataQualityCheckConfiguration();
     }
 
     @Bean
