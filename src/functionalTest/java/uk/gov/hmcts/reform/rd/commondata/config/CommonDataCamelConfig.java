8f13d5657283157c41d60028cd4e41f9d14614ff
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-01-12 09:58:48 +0000

    DTSRD-72: checkstyle issues

--- src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/config/CommonDataCamelConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/config/CommonDataCamelConfig.java
@@ -10,7 +10,6 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.testcontainers.containers.PostgreSQLContainer;
