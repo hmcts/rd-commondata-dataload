6859ee2a91951186ec9120d8907d2ddfd208a943
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2024-10-28 16:01:51 +0000

    DTSRD-72: Spring boot upgrade, fixed camel and spring batch issues

--- src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/config/CommonDataCamelConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/rd/commondata/config/CommonDataCamelConfig.java
@@ -10,7 +10,6 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
