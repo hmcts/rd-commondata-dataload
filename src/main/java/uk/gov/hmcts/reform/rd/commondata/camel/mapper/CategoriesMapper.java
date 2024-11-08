6ba6384e33b6cb15348eda1995452994cee086ea
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-11-08 13:17:11 +0000

    DTSRD-3529. add 2 extra columns in the LOV table

--- src/main/java/uk/gov/hmcts/reform/rd/commondata/camel/mapper/CategoriesMapper.java
+++ src/main/java/uk/gov/hmcts/reform/rd/commondata/camel/mapper/CategoriesMapper.java
@@ -15,7 +15,7 @@ public class CategoriesMapper implements IMapper {
 
         return CommonMapper.getMap(categories.getCategoryKey(), categories.getServiceId(), categories.getKey(),
             categories.getValueEN(), categories.getValueCY(), categories.getHintTextEN(), categories.getHintTextCY(),
-            categories.getParentCategory(), categories.getParentKey(), categories.getActive(),
-            categories.getLovOrder());
+            categories.getParentCategory(), categories.getParentKey(), categories.getActive(),categories.getExternalReference(),
+                                   categories.getExternalReferenceType(), categories.getLovOrder());
     }
 }
---
+++ src/main/resources/db/migration/V1_8__alter_List_Of_Values.sql
@@ -0,0 +1,3 @@
+
+ALTER TABLE List_Of_Values ADD COLUMN external_reference VARCHAR(200);
+ALTER TABLE List_Of_Values ADD COLUMN external_reference_type VARCHAR(200);
