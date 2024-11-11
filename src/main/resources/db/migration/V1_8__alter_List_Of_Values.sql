bcbb740b11a2ca377c9c743f12d666820de343f6
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-11-11 11:45:48 +0000

    DTSRD-3529. add 2 extra columns in the LOV table

--- src/main/resources/db/migration/V1_8__alter_List_Of_Values.sql
+++ src/main/resources/db/migration/V1_8__alter_List_Of_Values.sql
@@ -1,3 +1,8 @@
 
 ALTER TABLE List_Of_Values ADD COLUMN external_reference VARCHAR(200);
 ALTER TABLE List_Of_Values ADD COLUMN external_reference_type VARCHAR(200);
+
+ALTER TABLE list_of_values
+ ADD constraint unique_external_reference check
+ ((external_reference_type is null and external_reference is null)
+ or (external_reference_type is not null and external_reference is not null))
